f041e747452730892693686888f56cb6a214ffd9
Matthew Elwin <elwin@northwestern.edu>
2023-09-22 23:18:12 -0500

    Sucessfully capture a frame

--- capture.c
+++ capture.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
@@ -35,10 +36,14 @@ int main(void)
            caps.bus_info,
            caps.version
         );
-    // Other capabilities are in the caps.capabilities field. We mainly care about video capture
-    printf("Capabilities: 0x%x, Video Capture Bit: %d\n",
+    // Other capabilities are in the caps.capabilities field.
+    printf("Capabilities: 0x%x\n"
+           "\tVideo Capture Bit: %u\n"
+           "\tStreaming: %u\n",
            caps.capabilities,
-           caps.capabilities & V4L2_CAP_VIDEO_CAPTURE);
+           !!(caps.capabilities & V4L2_CAP_VIDEO_CAPTURE),
+           !!(caps.capabilities & V4L2_CAP_STREAMING)
+        );
 
     printf("\nGetting Video Formats\n");
     // https://docs.kernel.org/userspace-api/media/v4l/vidioc-g-fmt.html#vidioc-g-fmt
@@ -77,43 +82,62 @@ int main(void)
     }
 
     // map the buffer into memory so this process can access it
-    struct v4l2_buffer buf =
+    struct v4l2_buffer buf_info =
         {
             .type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
             .memory = V4L2_MEMORY_MMAP,
             .index = 0
         };
-    if(-1 == ioctl(fd, VIDIOC_QUERYBUF, & buf))
+
+    if(-1 == ioctl(fd, VIDIOC_QUERYBUF, &buf_info))
     {
         perror("Error Querying Buffer");
         exit(EXIT_FAILURE);
     }
-    printf("Buffer size is %d\n", buf.length);
+    printf("Buffer size is %d\n", buf_info.length);
 
     printf("Allocated the memory-mapped buffer\n");
-    u_int8_t * buffer = NULL;
-    buffer = (u_int8_t*)mmap(NULL,
-                             buf.length,
-                             PROT_READ | PROT_WRITE,
-                             MAP_SHARED,
-                             fd,
-                             buf.m.offset);
+    void * buffer = mmap(NULL,
+                         buf_info.length,
+                         PROT_READ | PROT_WRITE,
+                         MAP_SHARED,
+                         fd,
+                         buf_info.m.offset);
 
     if(MAP_FAILED == buffer)
     {
         perror("Error mapping memory");
         exit(EXIT_FAILURE);
     }
+    // clear the buffer to be safe
+    memset(buffer, 0, buf_info.length);
 
+    // Now ready to start the stream and grab a frame
     printf("Beginning the stream\n");
-    if(-1 == ioctl(fd, VIDIOC_STREAMON, &buf.type))
+    u_int32_t type = buf_info.type;
+    if(-1 == ioctl(fd, VIDIOC_STREAMON, &type))
     {
         perror("Could not begin stream.");
         exit(EXIT_FAILURE);
     }
 
-    // Grab a frame.
-    // Wait for a frame to be available
+    // With streaming active we can now grab a frame
+    printf("Enqueueing a buffer.\n");
+    // Enqueue the buffer. Make a new struct so it is cleared
+    struct v4l2_buffer buf_info1 =
+        {
+            .type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+            .memory = V4L2_MEMORY_MMAP,
+            .index = 0
+        };
+    if(-1 == ioctl(fd, VIDIOC_QBUF, &buf_info1))
+    {
+        perror("Could not enqueue buffer");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Waiting for data\n");
+
     fd_set fds;
     FD_ZERO(&fds);
     FD_SET(fd, &fds);
@@ -124,9 +148,16 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    printf("Grabbing the frame\n");
+
+    printf("Reading the frame\n");
+    struct v4l2_buffer buf1 =
+        {
+            .type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+            .memory = V4L2_MEMORY_MMAP
+        };
+
     // Grab the frame
-    if(-1 == ioctl(fd, VIDIOC_DQBUF, &buf))
+    if(-1 == ioctl(fd, VIDIOC_DQBUF, &buf1))
     {
         perror("Error grabbing frame.");
         exit(EXIT_FAILURE);
@@ -143,7 +174,7 @@ int main(void)
         exit(EXIT_FAILURE);
     }
     printf("Writing frame to %s\n", tmpfile_name);
-    if( -1 == write(tmpfile, buffer, buf.length))
+    if( -1 == write(tmpfile, buffer, buf_info1.length))
     {
         perror("Error writing to tmpfile.");
         exit(EXIT_FAILURE);
