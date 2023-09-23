fde5d755e0918efe9b9847a0daa1c16bf6048219
Matthew Elwin <elwin@northwestern.edu>
2023-09-22 22:29:23 -0500

    Hangs on actually capturing the frame

--- capture.c
+++ capture.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <linux/videodev2.h>
@@ -63,39 +64,40 @@ int main(void)
     // There are other streaming methods to try but
     // mmap streaming seems to be the most common
     // https://docs.kernel.org/userspace-api/media/v4l/vidioc-reqbufs.html#vidioc-reqbufs
-    struct v4l2_requestbuffers req_buf =
+    struct v4l2_requestbuffers req =
         {
             .count = 1,
             .type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
             .memory = V4L2_MEMORY_MMAP,
         };
-    if(-1 == ioctl(fd, VIDIOC_REQBUFS, &req_buf))
+    if(-1 == ioctl(fd, VIDIOC_REQBUFS, &req))
     {
         perror("Error Requesting Buffers.");
         exit(EXIT_FAILURE);
     }
 
     // map the buffer into memory so this process can access it
-    struct v4l2_buffer buf_info =
+    struct v4l2_buffer buf =
         {
             .type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
             .memory = V4L2_MEMORY_MMAP,
             .index = 0
         };
-    if(-1 == ioctl(fd, VIDIOC_QUERYBUF, & buf_info))
+    if(-1 == ioctl(fd, VIDIOC_QUERYBUF, & buf))
     {
         perror("Error Querying Buffer");
         exit(EXIT_FAILURE);
     }
+    printf("Buffer size is %d\n", buf.length);
 
     printf("Allocated the memory-mapped buffer\n");
     u_int8_t * buffer = NULL;
     buffer = (u_int8_t*)mmap(NULL,
-                             buf_info.length,
+                             buf.length,
                              PROT_READ | PROT_WRITE,
                              MAP_SHARED,
                              fd,
-                             buf_info.m.offset);
+                             buf.m.offset);
 
     if(MAP_FAILED == buffer)
     {
@@ -104,13 +106,48 @@ int main(void)
     }
 
     printf("Beginning the stream\n");
-    unsigned int stream_type = req_buf.type;
-    if(-1 == ioctl(fd, VIDIOC_STREAMON, &stream_type))
+    if(-1 == ioctl(fd, VIDIOC_STREAMON, &buf.type))
     {
         perror("Could not begin stream.");
         exit(EXIT_FAILURE);
     }
 
+    // Grab a frame.
+    // Wait for a frame to be available
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+    struct timeval timeout = { .tv_sec = 2 };
+    if( -1 == select(fd + 1, &fds, NULL, NULL, &timeout))
+    {
+        perror("Error waiting for frame.");
+        exit(EXIT_FAILURE);
+    }
 
+    printf("Grabbing the frame\n");
+    // Grab the frame
+    if(-1 == ioctl(fd, VIDIOC_DQBUF, &buf))
+    {
+        perror("Error grabbing frame.");
+        exit(EXIT_FAILURE);
+    }
+
+
+    printf("Creating temporary output file\n");
+    // write the frame to a temporary file
+    char tmpfile_name[]="video-XXXXXX";
+    int tmpfile = mkstemp(tmpfile_name);
+    if(-1 == tmpfile )
+    {
+        perror("Error creating temporary output file.");
+        exit(EXIT_FAILURE);
+    }
+    printf("Writing frame to %s\n", tmpfile_name);
+    if( -1 == write(tmpfile, buffer, buf.length))
+    {
+        perror("Error writing to tmpfile.");
+        exit(EXIT_FAILURE);
+    }
+    printf("Frame capture complete.\n");
     return 0;
 }
